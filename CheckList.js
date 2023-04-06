41da95d39112ee575109c3d1e25493d14f2f3466
QUADEH <127901701+QUADEH@users.noreply.github.com>
2023-04-05 20:05:51 -0500

    Update CheckList.js

--- CheckList.js
+++ CheckList.js
@@ -2,11 +2,11 @@ const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
 let lastChecked;
 
-function handleCheck(e) {
+function handleCheck(event) {
   // Check if they had the shift key down
   // AND check that they are checking it
   let inBetween = false;
-  if (e.shiftKey && this.checked) {
+  if (event.shiftKey && this.checked) {
     // go ahead and do what we please
     // loop over every single checkbox
     checkboxes.forEach(checkbox => {
